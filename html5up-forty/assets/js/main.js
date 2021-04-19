b8a849fd1d183bf17e198f9967cebc4eb952a64e
Loveyvish <69102084+Loveyvish@users.noreply.github.com>
2021-04-19 22:47:19 +0530

    done

--- html5up-forty/assets/js/main.js
+++ html5up-forty/assets/js/main.js
@@ -364,3 +364,34 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
+//Button
+
+function toggle(){
+	var blur = document.getElementById('blur');
+	blur.classList.toggle('active');
+	// var blur2 = document.getElementById('blur2');
+	// blur2.classList.toggle('active');
+	// var blur3 = document.getElementById('blur3');
+	// blur3.classList.toggle('active');
+	// var blur4 = document.getElementById('blur4');
+	// blur4.classList.toggle('active');
+	// var blur5 = document.getElementById('blur5');
+	// blur5.classList.toggle('active');
+	var popup = document.getElementById('popup');
+	popup.classList.toggle('active');
+	
+}
+
+function toggle2(){
+	var blur = document.getElementById('blur');
+	blur.classList.toggle('active');
+	var popup2 = document.getElementById('popup2');
+	popup2.classList.toggle('active');
+}
+
+function toggle3(){
+	var blur = document.getElementById('blur');
+	blur.classList.toggle('active');
+	var popup3 = document.getElementById('popup3');
+	popup3.classList.toggle('active');
+}
